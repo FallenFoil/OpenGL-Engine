aa0ca009d3c88321186e3f021602744a96091b70
FallenFoil <luisjrsm@hotmail.com>
2019-04-18 16:01:33 +0100

    Patches de Bezier

--- Generator/main.cpp
+++ Generator/main.cpp
@@ -1,15 +1,21 @@
-#include <iostream>
-#include <fstream>
 #include <string.h>
 #include <stdlib.h>
-#include "Ponto.h"
+#include <iostream>
+#include <fstream>
 #include <sstream>
+#include <vector>
+#include "Ponto.h"
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 using namespace std;
 
-float *vertexSphere = new float[1024]();
-int vertexSize=0;
+//Patch variables
+int nPatches=0;
+vector<vector<int>> indexes;
+int nPoints=0;
+vector<string> points;
+int resU,resV;
 
 
 string createBox(float width, float height, float depth,  int stacks, int slices, int depthDivisions){
@@ -59,7 +65,6 @@ string createBox(float width, float height, float depth,  int stacks, int slices
     return ss.str();
 }
 
-
 /*
  * Calcula as coordenadas dos vertices de um plano.
  *
@@ -70,9 +75,6 @@ string createBox(float width, float height, float depth,  int stacks, int slices
  *
  * Retorna uma string com os vertice do plano
  */
-
-// A B
-// D C
 string createPlane(float oX, float oY, float oZ, float sizeX, float sizeY, float sizeZ, int direction){
     float x, y=0, z;
     stringstream ss;
@@ -84,7 +86,7 @@ string createPlane(float oX, float oY, float oZ, float sizeX, float sizeY, float
 
 
     //Triangulo esquerdo
-    ss << "##FAN" << endl;
+    ss << "##Strips" << endl;
 
 
     //A
@@ -120,8 +122,6 @@ string createPlane(float oX, float oY, float oZ, float sizeX, float sizeY, float
 
     return ss.str();
 }
-
-
 
 /*
  * Transforma coordenadas esfericas em cartesianas
@@ -194,7 +194,7 @@ string createSphere(float radius, int slices, int stacks){
  *
  * Retorna uma string com a coordenadas cartesianas do ponto.
  */
-    string createConePoint(float alpha, float radius, float height){
+string createConePoint(float alpha, float radius, float height){
     float x,y,z;
     stringstream ss;
 
@@ -209,7 +209,6 @@ string createSphere(float radius, int slices, int stacks){
     return ss.str();
 }
 
-
 /*
  * Calcula os pontos de um cone construido por triangulos
  *
@@ -217,7 +216,6 @@ string createSphere(float radius, int slices, int stacks){
  *
  * Retorna uma string com todos os pontos do cone.
  */
-
 string createCone(double radius, double height, int slices, int stacks){
     if(slices < 3 || stacks < 3) return string();
 
@@ -263,6 +261,156 @@ string createCone(double radius, double height, int slices, int stacks){
         }
 
     return ss.str();
+}
+
+void multMatrixVector(float *m, int l, float *v, float *res) {
+    for(int j = 0; j < l; ++j){
+        res[j] = 0;
+        for(int k = 0; k < 4; ++k){
+            res[j] += v[k] * m[j * 4 + k];
+        }
+    }
+}
+
+void multVectorMatrix(float *v, float *m, float *res) {
+    for(int j = 0; j < 4; ++j){
+        res[j] = 0;
+        for(int k = 0; k < 4; ++k){
+            res[j] += v[k] * m[k * 3 + j + k];
+        }
+    }
+}
+
+void fillBigP(int p, float bigPX[], float bigPY[], float bigPZ[]){
+    vector<int> indexAux = indexes.at(p);
+    for (int i = 0; i < 16; ++i) {
+        istringstream ss(points.at(indexAux.at(i)));
+        string s;
+
+        getline(ss, s, ',');
+        bigPX[i]=strtof(s.c_str(),0);
+
+        getline(ss, s, ',');
+        bigPY[i]=strtof(s.c_str(),0);
+
+        getline(ss, s, ',');
+        bigPZ[i]=strtof(s.c_str(),0);
+    }
+}
+
+string getBezier(float ru, float rv, int p){
+    float v = 1.0 * rv / (resV-1);
+    float u = 1.0 * ru / (resU-1);
+    float bigPX[4*4];
+    float bigPY[4*4];
+    float bigPZ[4*4];
+    float bigBX[1];
+    float bigBY[1];
+    float bigBZ[1];
+    float part1[4], part2[4];
+    float bigU[] = {1, u, u*u, u*u*u};
+    float bigV[] = {1, v, v*v, v*v*v};
+
+    float bigM[] = {  1, 0, 0,0,
+                      -3, 3, 0,0,
+                      3,-6, 3,0,
+                      -1, 3,-3,1};
+    float bigMTrans[] = {  1,-3, 3,-1,
+                           0, 3,-6, 3,
+                           0, 0, 3,-3,
+                           0, 0, 0, 1};
+
+    multVectorMatrix(bigU, bigM, part1);
+    multMatrixVector(bigMTrans, 4, bigV, part2);
+
+    fillBigP(p, bigPX, bigPY, bigPZ);
+
+    float auxX[4];
+    float auxY[4];
+    float auxZ[4];
+
+    multMatrixVector(bigPX, 4, part2, auxX);
+    multMatrixVector(bigPY, 4, part2, auxY);
+    multMatrixVector(bigPZ, 4, part2, auxZ);
+
+    multMatrixVector(part1, 1, auxX, bigBX);
+    multMatrixVector(part1, 1, auxY, bigBY);
+    multMatrixVector(part1, 1, auxZ, bigBZ);
+
+    stringstream ss;
+    ss << bigBX[0] << " " << bigBY[0] << " " << bigBZ[0];
+
+    return ss.str();
+}
+
+string createBezierPatches(){
+    stringstream ss1;
+    stringstream ss2;
+    int pointscount = 0;
+    ss2 << "##Triangles" << endl;
+
+    for (int p = 0; p < nPatches; p++) {
+        for (int u = 0; u < resU-1; u++) {
+            for (int v = 0; v < resV-1; v++) {
+                ss2 << getBezier(u, v, p) << endl;
+                ss2 << getBezier(u, v+1, p) << endl;
+                ss2 << getBezier(u+1, v, p) << endl;
+
+                ss2 << getBezier(u, v+1, p) << endl;
+                ss2 << getBezier(u+1, v+1, p) << endl;
+                ss2 << getBezier(u+1, v, p) << endl;
+                pointscount += 6;
+            }
+        }
+    }
+
+    ss1 << pointscount << endl;
+    ss1 << ss2.str();
+
+    return ss1.str();
+}
+
+string loadPatch(char* file, int tessLevel){
+    string line;
+    ifstream myFile(file);
+
+    if (myFile.is_open()){
+        getline(myFile,line);
+        const char* str = line.c_str();
+        nPatches = atoi(str);
+
+        for(int i = 0; i < nPatches; ++i){
+            vector<int> indexPatch;
+            getline(myFile,line);
+            istringstream ss(line);
+            string s;
+
+            while(getline(ss,s,',')){
+                indexPatch.push_back(atoi(s.c_str()));
+            }
+            indexes.push_back(indexPatch);
+        }
+
+        getline(myFile,line);
+        str = line.c_str();
+        nPoints = atoi(str);
+
+        for(int i = 0; i < nPoints; ++i){
+            getline(myFile,line);
+            points.push_back(line);
+        }
+        myFile.close();
+
+        resU=tessLevel;
+        resV=tessLevel;
+
+        return createBezierPatches();
+    }
+    else{
+        cout << "Unable to open patch file";
+    }
+
+    return "";
 }
 
 int main(int argc, char** argv){
@@ -346,8 +494,17 @@ int main(int argc, char** argv){
         file << createCone(atof(argv[2]), atof(argv[3]), atof(argv[4]), atof(argv[5]));
     }
     else{
-        printf("Wrong shape\n");
-        exit(EXIT_FAILURE);
+        if(argc<3){
+            printf("Required Bezier Patches file and tessellation level.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        if(argc<4){
+            printf("Required output file name.\n");
+            exit(EXIT_FAILURE);
+        }
+        file.open(argv[3]);
+        file << loadPatch(argv[1], atoi(argv[2]));
     }
     return 0;
 }
